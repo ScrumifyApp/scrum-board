3544402a734813b4daf8a1bd871438889b612154
Mark Teets <markteets@gmail.com>
2023-05-22 02:13:20 -0600

    Team create/join now adds new team to userContext

--- client/pages/UserHome/UserHomePage.jsx
+++ client/pages/UserHome/UserHomePage.jsx
@@ -1,16 +1,37 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { userContext, teamContext } from '../../context';
+import { userContext, teamContext, pageContext } from '../../context';
 import { useNavigate } from 'react-router-dom';
-
 import TeamDisplay from './components/TeamDisplay';
+
 
 const UserHomePage = () => {
 	const { user, setUser } = useContext(userContext);
-	const { team, setTeam } = useContext(teamContext);
+  const { team, setTeam } = useContext(teamContext);
+  const { lastPage } = useContext(pageContext);
 	const [joinTeamCode, setJoinTeamCode] = useState('');
 	const [newTeamName, setNewTeamName] = useState('');
 	const [teamArray, setTeamArray] = useState([]);
 	const navigate = useNavigate();
+  
+  // Set lastPage to variable that will prevent automatic page jump if page has just loaded
+  useEffect(() => {
+    lastPage.current = 'JustLoadedUserHomePage';
+  }, [])
+
+  // Navigate to ScrumBoardPage after team has successfully been updated from either 
+  // creating a team, joining a team, or selecting an existing team
+  useEffect(() => {
+    // Make sure the team has been set and the user didn't just get to this page before navigating to scrumboard
+    if (team !== null && lastPage.current !== 'JustLoadedUserHomePage') {
+      return navigate('/ScrumBoardPage');
+    }
+  }, [team])
+
+	//Update teamArray displayed below after user has been loaded from global context
+	useEffect(() => {
+    setTeamArray(makeTeamArray(user?.userTeams));
+  }, [user]);
+
 
 	// Function executes when the user clicks the "Create New Team" button.
 	// Takes the input from the associated input field, creates a team with that input as the team name,
@@ -35,14 +56,18 @@ const UserHomePage = () => {
 			}),
 		});
 
-		// console.log('repsonse:', response);
 		if (response.status === 200) {
-			const res = await response.json();
+      const res = await response.json();
+      // update user to include new team in teams array 
+      setUser({
+        ...user,
+        userTeams: res.user.userTeams
+      });
 			//Updates global team context to the team_id that was just created
-			setTeam(res.team_id);
-			// console.log('team_id:', res.team_id);
-			//navigates to scrumboard
-			return navigate('/ScrumBoardPage');
+      setTeam(Number(res.team_id));
+      // update last page to this page before navigating away from it
+      lastPage.current = '/UserHomePage';
+      return;
 		}
 
 		alert('Server fail');
@@ -59,7 +84,8 @@ const UserHomePage = () => {
 			return;
 		}
 		// console.log('join team code:', joinTeamCode);
-		//send a post request to the backend to add current context user to team from url
+		// send a post request to the backend to add current context user to team from url
+    // Using param to make it easier to transistion from using the team_id number to a more complex link
 		const response = await fetch(`/api/user/join-team/${joinTeamCode}`, {
 			method: 'POST',
 			headers: {
@@ -72,29 +98,42 @@ const UserHomePage = () => {
 		if (response.status === 200) {
 			const res = await response.json();
 			//check variable sent back to see if add was successful
-			if (res.teamAdded) {
-				// console.log(user.username, 'was successfully added to the team!');
-				//Updates global team context to the team_id that was just created
-				setTeam(res.team_id);
-				// console.log('team_id:', res.team_id);
-				//navigates to scrumboard
-				return navigate('/ScrumBoardPage');
-			}
-			//There's probably a smoother way to make this happen. Maybe adding a stateful
-			//message in the page
-			alert(
-				'Server could not add you to the team because the team url was invalid'
-			);
-			return;
+      switch (res.teamAddStatus) {
+        case ('Success'): {
+        // update user to include new team in teams array 
+        setUser({
+          ...user,
+          userTeams: res.user.userTeams
+        });
+        //Updates global team context to the team_id that was just created
+			  setTeam(Number(res.team_id));
+				// update last page to this page before navigating away from it
+        lastPage.current = '/UserHomePage';
+        return;  
+        }
+        case ('AlreadyMember'): {
+          //There's probably a smoother way to make this happen. Maybe adding a stateful message in the page
+          alert(
+            'You are already a member of this team'
+          );
+          return;
+        }
+        case ('InvalidURL'): {
+          alert(
+            'This team url is invalid, please try a different url'
+          );
+          return;
+        }
+        default: {
+          console.log('The UserHomePage switch case failed to find a match before the default case')
+          return;
+        }
+      }
 		}
 		alert('Server fail');
 		return;
 	};
 
-	//Update teamArray displayed below after user has been loaded from global context
-	useEffect(() => {
-		setTeamArray(makeTeamArray(user.userTeams));
-	}, [user]);
 
 	//What renders from the funcitonal component
 	return (
@@ -128,11 +167,12 @@ const UserHomePage = () => {
 
 export default UserHomePage;
 
+
 function makeTeamArray(userTeamArray) {
 	if (!userTeamArray || userTeamArray.length === 0) {
-		return <div>Join some teams already!!! Don't you have friends?</div>;
+		return <div>Please create or join a team</div>;
 	}
-	return userTeamArray.map((team) => {
-		return <TeamDisplay teamName={team.team_name} teamId={team.id} />;
+  return userTeamArray.map((userTeam) => {
+		return <TeamDisplay userTeamName={userTeam.team_name} userTeamId={userTeam.id} />;
 	});
 }
