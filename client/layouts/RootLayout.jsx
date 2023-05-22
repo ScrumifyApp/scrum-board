0ec4edc01447772aad263edc77defc8acb752e0e
Mark Teets <markteets@gmail.com>
2023-05-22 01:42:30 -0600

    Made the navbar dynamic based on current page

--- client/layouts/RootLayout.jsx
+++ client/layouts/RootLayout.jsx
@@ -1,19 +1,16 @@
-import React from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import React, {useContext} from "react";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
+import { userContext, pageContext } from "../context";
 
 const RootLayout = () => {
+  const { setUser } = useContext(userContext);
+  const { lastPage } = useContext(pageContext);
+  const { pathname: locationURL } = useLocation();
 
   return (
     <div className="root-layout">
       <header>
-        {/* <h1>Root Layout</h1> */}
-        <nav id='main-nav'>
-          {/* <h1>I'm for testing page navigation, the links aren't actually meant for user navigation</h1> */}
-          <NavLink to='/' className='nav-link'> Login Page </NavLink>
-          <NavLink to='/SignUpPage'className='nav-link'> Sign-up Page </NavLink>
-          <NavLink to='/UserHomePage'className='nav-link'> User Home Page </NavLink>
-          <NavLink to='/ScrumBoardPage'className='nav-link'> Scrum Board Page </NavLink>
-        </nav>
+        {formatNavBar(locationURL, lastPage, setUser)}
       </header>
       <main>
         <Outlet />
@@ -24,3 +21,58 @@ const RootLayout = () => {
 };
 
 export default RootLayout;
+
+const formatNavBar = (locationURL, lastPage, setUser) => {
+  switch (locationURL) {
+    case '/': {
+      return (
+        <nav id='main-nav'>
+          <NavLink to='/SignUpPage' className='nav-link' > Sign-up Page </NavLink>
+        </nav>
+      )
+    }
+    case '/SignUpPage': {
+      return (
+        <nav id='main-nav'>
+          <NavLink to='/' className='nav-link' > Login Page </NavLink>
+        </nav>
+      )
+    }
+    case '/UserHomePage': {
+      return (
+        <nav id='main-nav'>
+          {lastPage.current === '/ScrumBoardPage' && <NavLink to='/ScrumBoardPage' className='nav-link' > Scrum Board Page </NavLink>}
+          <NavLink to='/' className='nav-link' onClick={() => setUser(null)} > Log Out </NavLink>
+        </nav>
+      )
+    }
+    case '/ScrumBoardPage': {
+      return (
+        <nav id='main-nav'>
+          <NavLink to='/UserHomePage' className='nav-link' > User Home Page </NavLink>
+          <NavLink to='/' className='nav-link' onClick={() => setUser(null)} > Log Out </NavLink>
+        </nav>
+      )
+    }
+    default: {
+      return (
+        // If you see links to both of the below page at once, it means none of the other paths match the currrent location
+        <nav id='main-nav'>
+          <NavLink to='/' className='nav-link' > Login Page </NavLink>
+          <NavLink to='/SignUpPage' className='nav-link' > Sign-up Page </NavLink>
+        </nav>
+      )
+    }
+  }
+}
+
+/* former navbar including all pages for testing:
+    <nav id='main-nav'>
+          <NavLink to='/' className='nav-link' > Login Page </NavLink>
+          <NavLink to='/SignUpPage'className='nav-link' > Sign-up Page </NavLink>
+          <NavLink to='/UserHomePage' className='nav-link' > User Home Page </NavLink>
+          <NavLink to='/ScrumBoardPage' className='nav-link' > Scrum Board Page </NavLink>
+          <NavLink to='/' className='nav-link' > Login Page </NavLink>
+        </nav>
+
+*/
